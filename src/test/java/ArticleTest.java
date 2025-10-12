7d0201bda7f04e60cb76210361c35c243df3065d
alexxxlim <22alex.lim.work22@gmail.com>
2025-10-12 22:28:10 +0200

    Version Final de los tests en ArticleTest.java
    he terminado con los tests para ArticleTest.java
    he hecho los tests para metodos:
    - getGrossAmount()
    - getDiscountedAmount()

--- src/test/java/ArticleTest.java
+++ src/test/java/ArticleTest.java
@@ -74,6 +74,76 @@ public class ArticleTest {
         double secondCall = articleGetGrossAmountRepeatability.getGrossAmount();
         assertEquals(firstCall, secondCall, EPS);
     }
-    //get discounted amount
 
+    //get discounted amount
+    private final Article articleGetDiscountedAmountZeroDiscount = new Article("ArticleForTest", 3, 10.0, 0.0);
+
+    @Test
+    void getDiscountedAmount_zeroDiscount() {
+        assertEquals(30.0, articleGetDiscountedAmountZeroDiscount.getDiscountedAmount(), EPS);
+    }
+
+    private final Article getArticleGetDiscountedAmountNormalCase = new Article("ArticleForTest", 2, 50.0, 15.00);
+
+    @Test
+    void getDiscountedAmount_normalCase() {
+        assertEquals(85.0, getArticleGetDiscountedAmountNormalCase.getDiscountedAmount(), EPS);
+    }
+
+    private final Article getArticleGetDiscountedAmountFullDiscount = new Article("ArticleForTest", 5, 12.0, 100.00);
+
+    @Test
+    void getDiscountedAmount_fullDiscount() {
+        assertEquals(0.0, getArticleGetDiscountedAmountFullDiscount.getDiscountedAmount(), EPS);
+    }
+
+    private final Article getArticleGetDiscountedAmountDecimalPrice = new Article("ArticleForTest", 7, 5.97, 3.0);
+
+    @Test
+    void getDiscountedAmount_decimalPrice() {
+        assertEquals(40.5363, getArticleGetDiscountedAmountDecimalPrice.getDiscountedAmount(), EPS);
+    }
+
+    //P.S. Aquí he probado nueva metodología del testing para mí
+    private final Article getArticleGetDiscountedAmountMonotoncityLOW = new Article("ArticleForTest", 2, 50.0, 10.0);
+    private final Article getArticleGetDiscountedAmountMonotoncityHIGH = new Article("ArticleForTest", 2, 50.0, 20.0);
+
+    @Test
+    void getDiscountedAmount_monotonicity() {
+        double low = getArticleGetDiscountedAmountMonotoncityLOW.getDiscountedAmount();
+        double high = getArticleGetDiscountedAmountMonotoncityHIGH.getDiscountedAmount();
+        assertEquals(true, low > high);
+    }
+
+    private final Article getArticleGetDiscountedAmountEdgesMIN = new Article("ArticleForTest", 1, 100.0, 0.0001);
+    private final Article getArticleGetDiscountedAmountEdgesMAX = new Article("ArticleForTest", 1, 100.0, 99.9999);
+
+    @Test
+    void getDiscountedAmount_edges() {
+        assertEquals(99.9999, getArticleGetDiscountedAmountEdgesMIN.getDiscountedAmount(), EPS);
+        assertEquals(0.0001, getArticleGetDiscountedAmountEdgesMAX.getDiscountedAmount(), EPS);
+    }
+
+    private final Article getArticleGetDiscountedAmountNegativeDiscount = new Article("ArticleForTest", 1, 10.0, -1.0);
+
+    @Test
+    void getDiscountedAmount_negatveDiscount() {
+        assertThrows(IllegalArgumentException.class, getArticleGetDiscountedAmountNegativeDiscount::getDiscountedAmount);
+    }
+
+    private final Article getArticleGetDiscountedAmountTooHighDiscount = new Article("ArticleForTest", 1, 10.0, 100.0001);
+
+    @Test
+    void getDiscountedAmount_invalidHigh() {
+        assertThrows(IllegalArgumentException.class, getArticleGetDiscountedAmountTooHighDiscount::getDiscountedAmount);
+    }
+
+    //P.S. Aquí he probado nueva metodología del testing para mí
+    private final Article getArticleGetDiscountedAndGetGrossAmountInvariant = new Article("ArticleForTest", 3, 10.0, 15.0);
+    @Test
+    void getDiscountedAmount_invariant() {
+        double gross = getArticleGetDiscountedAndGetGrossAmountInvariant.getGrossAmount();
+        double discounted = getArticleGetDiscountedAndGetGrossAmountInvariant.getDiscountedAmount();
+        assertEquals(false, discounted > gross);
+    }
 }
